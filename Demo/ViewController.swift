a5ea8bb18445c92c8f7b392804458721e9364a97
Jason R Tibbetts <jason@tibbetts.net>
2018-03-08 16:33:22 -0500

    Added a new target for the demo app.

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONClientDemo
+//
+//  Created by Jason R Tibbetts on 3/8/18.
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
