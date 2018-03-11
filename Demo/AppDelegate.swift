603ac8703dfa12c877ae2bef45c752f062b722cd
Jason R Tibbetts <jason@tibbetts.net>
2018-03-10 20:42:36 -0500

    003: Added a callback URL for OAuth projects.

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -1,10 +1,12 @@
 //  Copyright © 2018 Poikile Creations. All rights reserved.
 
+import OAuthSwift
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    public static let callbackUrlString = "jsonclientdemo://oauth-callback/github"
     var window: UIWindow?
 
 
@@ -33,6 +35,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        // The callback URL that was used when the OAuth app was created. In
+        // this case, the custom URL is "jsonclientdemo://oauth-callback/github".
+        if url.scheme == "jsonclientdemo" && url.host == "oauth-callback" {
+            OAuthSwift.handle(url: url)
+
+            return true
+        } else {
+            return false
+        }
     }
 
 
