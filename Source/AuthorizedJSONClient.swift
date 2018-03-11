80467f1fcdb0e6ffbc1954258deee914c6707da1
Jason R Tibbetts <jason@tibbetts.net>
2018-03-10 23:10:44 -0500

    003: Added a simple AuthorizedJSONClient subclass for GitHub, and a GitHubUser struct. 003: Removed the Discogs loading action in the demo.

--- Source/AuthorizedJSONClient.swift
+++ Source/AuthorizedJSONClient.swift
@@ -84,10 +84,19 @@ open class AuthorizedJSONClient: JSONClient {
     /// this once the user successfully authenticates with the server.
     var oAuthClient: OAuthSwiftClient?
 
+    /// The OAuth authentication mode that the client will use for
+    /// authorization. This will be either `OAuth1Swift` or `OAuth2Swift`.
     var oAuth: OAuthSwift
 
     // MARK: - Initialization
 
+    /// Initialize the client with an OAuth type and an optional base URL.
+    ///
+    /// - parameter oAuth: The OAuth authentication mode. This will be either
+    ///             `OAuth1Swift` or `OAuth2Swift`.
+    /// - parameter baseUrl: The API URL that all paths will be resolved
+    ///             against. If it's `nil` (the default), then each path that's
+    ///             passed to the REST functions must be an absolute URL string.
     public init(oAuth: OAuthSwift,
                 baseUrl: URL? = nil) {
         self.oAuth = oAuth
@@ -96,6 +105,8 @@ open class AuthorizedJSONClient: JSONClient {
 
     // MARK: - REST methods
 
+    /// `GET` JSON data that requires client authentication to access and
+    /// return it as a `Promise` of the desired `Codable` data type.
     open func authorizedGet<T: Codable>(path: String,
                                         headers: Headers = [:],
                                         params: [String: Any] = [:]) -> Promise<T> {
