1d16db75ef1032a77b0f8593550d43240b5fad80
Jason R Tibbetts <jason@tibbetts.net>
2018-03-12 07:50:26 -0400

    003: Renamed OAuth2 to OAuth2JSONClient. Added lots of documentation.

--- Source/OAuth1JSONClient.swift
+++ Source/OAuth1JSONClient.swift
@@ -4,10 +4,32 @@ import OAuthSwift
 import PromiseKit
 import UIKit
 
+/// An `AuthorizedJSONClient` implementation that uses OAuth 1 for
+/// authentication and authorization.
 open class OAuth1JSONClient: AuthorizedJSONClient {
 
+    /// The OAuth engine. Note that there's already an `oAuth` property in the
+    /// superclass, and its type is `OAuthSwift`, which is the superclass of
+    /// `OAuth1Swift`. This one is here so that we don't have to cast the
+    /// `oAuth` property to the desired type.
     fileprivate let oAuth1: OAuth1Swift
 
+    /// Initialize the client with the app's hashes on the server, as well as
+    /// the server's various OAuth-related URLs.
+    ///
+    /// - parameter consumerKey: The hash that identifies the app that this
+    ///             client communicates with. It's also called a client key.
+    /// - parameter consumerSecret: The password hash for the server app. This
+    ///             value should not be included in publicly-visible code.
+    /// - parameter requestTokenUrl: The server endpoint for getting an OAuth
+    ///             request token.
+    /// - parameter authorizeUrl: The URL of the app's sign-in page.
+    /// - parameter accessTokenUrl: The URL where the OAuth client can exchange
+    ///             a request token for an access token.
+    /// - parameter baseUrl: The service's root-level URL. All relative paths
+    ///             that are passed to the various REST functions will be
+    ///             resolved against this URL. If it's `nil`, then all paths
+    ///             must be absolute URL strings.
     public init(consumerKey: String,
                 consumerSecret: String,
                 requestTokenUrl: String,
@@ -22,14 +44,25 @@ open class OAuth1JSONClient: AuthorizedJSONClient {
         super.init(oAuth: oAuth1, baseUrl: baseUrl)
     }
 
+    /// Launch the service's sign-in page in a modal Safari web view. After the
+    /// user has successfully authenticated, the web page will be redirected to
+    /// the callback URL, which is unique to the client application.
+    ///
+    /// - parameter presentingViewController: The view controller over which the
+    ///             Safari view controller will be presented modally.
+    /// - parameter callbackUrlString: The URL that the web view will load
+    ///             after authentication succeeds.
+    ///
+    /// - returns:  A `Promise` containing whatever type of data is sent back
+    ///             by the server after authentication succeeds.
     open func authorize<T>(presentingViewController: UIViewController,
                            callbackUrlString: String) -> Promise<T> {
         oAuth1.authorizeURLHandler = SafariURLHandler(viewController: presentingViewController, oauthSwift: oAuth)
 
-        return Promise<T>() { (fulfill, reject) in
-            _ = self.oAuth1.authorize(withCallbackURL: callbackUrlString,
-                                      success: { [weak self] (credentials, response, parameters) in
-                                        self?.oAuthClient = OAuthSwiftClient(credential: credentials)
+        return Promise<T>() { [weak self] (fulfill, reject) in
+            let _ = self?.oAuth1.authorize(withCallbackURL: callbackUrlString,
+                                           success: { [weak self] (credentials, response, parameters) in
+                                            self?.oAuthClient = OAuthSwiftClient(credential: credentials)
                 }, failure: { (error) in
                     reject(error)
             })
--- Source/OAuth2JSONClient.swift
+++ Source/OAuth2JSONClient.swift
@@ -3,8 +3,14 @@
 import OAuthSwift
 import PromiseKit
 
-open class OAuth2: AuthorizedJSONClient {
+/// An `AuthorizedJSONClient` implementation that uses OAuth 2 for
+/// authentication and authorization.
+open class OAuth2JSONClient: AuthorizedJSONClient {
 
+    /// The OAuth engine. Note that there's already an `oAuth` property in the
+    /// superclass, and its type is `OAuthSwift`, which is the superclass of
+    /// `OAuth2Swift`. This one is here so that we don't have to cast the
+    /// `oAuth` property to the desired type.
     fileprivate let oAuth2: OAuth2Swift
 
     /// A one-time random string value that's added to request headers and
@@ -12,6 +18,20 @@ open class OAuth2: AuthorizedJSONClient {
     /// made by the right entity.
     fileprivate let state: String = "\(arc4random_uniform(UINT32_MAX))"
 
+    /// Initialize the client with the app's hashes on the server, as well as
+    /// the server's various OAuth-related URLs.
+    ///
+    /// - parameter consumerKey: The hash that identifies the app that this
+    ///             client communicates with. It's also called a client key.
+    /// - parameter consumerSecret: The password hash for the server app. This
+    ///             value should not be included in publicly-visible code.
+    /// - parameter authorizeUrl: The URL of the app's sign-in page.
+    /// - parameter accessTokenUrl: The URL where the OAuth client can exchange
+    ///             a request token for an access token.
+    /// - parameter baseUrl: The service's root-level URL. All relative paths
+    ///             that are passed to the various REST functions will be
+    ///             resolved against this URL. If it's `nil`, then all paths
+    ///             must be absolute URL strings.
     public init(consumerKey: String,
                 consumerSecret: String,
                 authorizeUrl: String,
@@ -21,21 +41,35 @@ open class OAuth2: AuthorizedJSONClient {
                              consumerSecret: consumerSecret,
                              authorizeUrl: authorizeUrl,
                              accessTokenUrl: accessTokenUrl,
-                             responseType: "token")
+                             responseType: "token")  // will it always be "token"?
         super.init(oAuth: oAuth2, baseUrl: baseUrl)
     }
 
+    /// Launch the service's sign-in page in a modal Safari web view. After the
+    /// user has successfully authenticated, the web page will be redirected to
+    /// the callback URL, which is unique to the client application.
+    ///
+    /// - parameter presentingViewController: The view controller over which the
+    ///             Safari view controller will be presented modally.
+    /// - parameter callbackUrlString: The URL that the web view will load
+    ///             after authentication succeeds.
+    /// - parameter scope: The level of access that the client is requesting.
+    ///             See the server's documentation for what scopes are
+    ///             supported. The default value is an empty string.
+    ///
+    /// - returns:  A `Promise` containing whatever type of data is sent back
+    ///             by the server after authentication succeeds.
     open func authorize<T>(presentingViewController: UIViewController,
                            callbackUrlString: String,
                            scope: String = "") -> Promise<T> {
         oAuth2.authorizeURLHandler = SafariURLHandler(viewController: presentingViewController, oauthSwift: oAuth)
 
         return Promise<T>() { (fulfill, reject) in
-            _ = self.oAuth2.authorize(withCallbackURL: callbackUrlString,
-                                      scope: scope,
-                                      state: state,
-                                      success: { [weak self] (credentials, response, parameters) in
-                                        self?.oAuthClient = OAuthSwiftClient(credential: credentials)
+            let _ = self.oAuth2.authorize(withCallbackURL: callbackUrlString,
+                                          scope: scope,
+                                          state: state,
+                                          success: { [weak self] (credentials, response, parameters) in
+                                            self?.oAuthClient = OAuthSwiftClient(credential: credentials)
                 }, failure: { (error) in
                     reject(error)
             })
