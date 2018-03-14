bf8da13619aa857106b596ecd87016fe220420d1
Jason R Tibbetts <jason@tibbetts.net>
2018-03-13 20:32:22 -0400

    No ticket: Added tests for AuthorizedJSONClient.handleSuccessfulResponse().

--- Tests/AuthorizedJSONClientTests.swift
+++ Tests/AuthorizedJSONClientTests.swift
@@ -13,7 +13,16 @@ class AuthorizedJSONClientTests: JSONClientTests {
         return AuthorizedJSONClient(oAuth: oAuth, baseUrl: baseUrl)
     }
 
-    func testAuthorizedGetBeforeAuthorizationFails() {
+    func testAuthorizedGetWithPathBeforeAuthorizationFails() {
+        let client = validJSONClient() as! AuthorizedJSONClient
+        let promise: Promise<DiscogsInfo> = client.authorizedGet(path: "/some/path")
+        let errorMessage = "The call should have been rejected because the user isn't authorized"
+
+        assert(promise: promise,
+               wasUnauthorizedWithMessage: errorMessage)
+    }
+
+    func testAuthorizedGetWithUrlBeforeAuthorizationFails() {
         let client = validJSONClient() as! AuthorizedJSONClient
         let url = URL(string: "https://api.discogs.com")!
         let promise: Promise<DiscogsInfo> = client.authorizedGet(url: url)
@@ -42,6 +51,44 @@ class AuthorizedJSONClientTests: JSONClientTests {
 
         assert(promise: promise,
                wasUnauthorizedWithMessage: errorMessage)
+    }
+
+    func testHandleSuccessfulResponseOk() {
+        // Make up some valid data.
+        let url = URL(string: "https://api.discogs.com")!
+        let stats = DiscogsInfo.Stats(labels: 500, artists: 1000, releases: 1000)
+        let discogsInfo = DiscogsInfo(apiVersion: "1.0", documentationUrl: url, hello: "Hello, world", statistics: stats)
+
+        // Construct fake HTTP and OAuth responses.
+        let httpResponse = HTTPURLResponse(url: url, statusCode: 400, httpVersion: nil, headerFields: nil)!
+
+        do {
+            let oAuthResponse = try OAuthSwiftResponse(data: JSONEncoder().encode(discogsInfo), response: httpResponse, request: nil)
+            let client = validJSONClient() as! AuthorizedJSONClient
+            let unpackedInfo: DiscogsInfo = try client.handleSuccessfulResponse(oAuthResponse)
+            XCTAssertEqual(unpackedInfo.hello, discogsInfo.hello)
+        } catch {
+            XCTFail("Couldn't construct the expected object from the data: \(error.localizedDescription)")
+        }
+    }
+
+    func testHandleSuccessfulResponseWithMismatchedTypesThrows() {
+        // Make up some valid data.
+        let url = URL(string: "https://api.discogs.com")!
+        let stats = DiscogsInfo.Stats(labels: 500, artists: 1000, releases: 1000)
+        let discogsInfo = DiscogsInfo(apiVersion: "1.0", documentationUrl: url, hello: "Hello, world", statistics: stats)
+
+        // Construct fake HTTP and OAuth responses.
+        let httpResponse = HTTPURLResponse(url: url, statusCode: 400, httpVersion: nil, headerFields: nil)!
+
+        do {
+            let oAuthResponse = try OAuthSwiftResponse(data: JSONEncoder().encode(discogsInfo), response: httpResponse, request: nil)
+            let client = validJSONClient() as! AuthorizedJSONClient
+            let _: String = try client.handleSuccessfulResponse(oAuthResponse)
+            XCTFail("Expected an error to be thrown")
+        } catch {
+            // Sweet.
+        }
     }
 
     func assert<T: Codable>(promise: Promise<T>,
