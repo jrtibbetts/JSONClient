2e4a5427daf6e8c144c504a1e202fbeddd75a969
Jason R Tibbetts <jason@martiancraft.com>
2018-03-31 22:25:13 -0400

    016: Upgraded to Xcode 9.3 & Swift 4.1.

--- Tests/DiscogsInfo.swift
+++ Tests/DiscogsInfo.swift
@@ -3,21 +3,16 @@
 import Foundation
 
 public struct DiscogsInfo: Codable {
+
     public var apiVersion: String
     public var documentationUrl: URL
     public var hello: String
     public var statistics: Stats
-
-    fileprivate enum CodingKeys: String, CodingKey {
-        case apiVersion = "api_version"
-        case documentationUrl = "documentation_url"
-        case hello
-        case statistics
-    }
 
     public struct Stats: Codable {
         public var labels: Int
         public var artists: Int
         public var releases: Int
     }
+    
 }
