0a0f28e0a036c3b628a566e0f496881b23301af9
Jason R Tibbetts <jason@tibbetts.net>
2020-06-15 00:35:34 -0400

    No ticket: Added a test target to the Package file.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let pkg = Package(
     name: "JSONClient",
 
     platforms: [
-        .iOS(.v10)
+        .iOS(.v12)
     ],
 
     products: [
@@ -16,17 +16,23 @@ let pkg = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/jrtibbetts/Stylobate.git", .upToNextMajor(from: "0.32.3")),
-        .package(url: "https://github.com/OAuthSwift/OAuthSwift.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", .upToNextMajor(from: "6.12.0"))
+        .package(url: "https://github.com/jrtibbetts/Stylobate.git",
+                 .upToNextMajor(from: "0.32.3")),
+        .package(url: "https://github.com/OAuthSwift/OAuthSwift.git",
+                 .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/mxcl/PromiseKit.git",
+                 .upToNextMajor(from: "6.12.0"))
     ],
 
     targets: [
-        .target(
-            name: "JSONClient",
-            dependencies: ["Stylobate", "OAuthSwift", "PromiseKit"],
-            path: "Source"
-        )
+        .target(name: "JSONClient",
+                dependencies: ["Stylobate", "OAuthSwift", "PromiseKit"],
+                path: "Source"
+        ),
+        .testTarget(name: "JSONClientTests",
+                    dependencies: ["JSONClient"],
+                    path: "Tests"),
+
     ]
 )
 
