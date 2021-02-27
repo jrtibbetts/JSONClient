0983f07420b77fd9f8e3ab5f8bbea7aef2436826
Jason R Tibbetts <jason@tibbetts.net>
2021-02-27 02:52:51 -0500

    No ticket: [config] Got rid of Carthage and git submodules, and switched to Swift Package Manager. And it WORKS!

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let pkg = Package(
     name: "JSONClient",
 
     platforms: [
-        .iOS(.v12)
+        .iOS(.v14)
     ],
 
     products: [
@@ -17,7 +17,7 @@ let pkg = Package(
 
     dependencies: [
         .package(url: "https://github.com/jrtibbetts/Stylobate.git",
-                 .upToNextMajor(from: "0.32.3")),
+                 .branch("main")),
         .package(url: "https://github.com/OAuthSwift/OAuthSwift.git",
                  .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/mxcl/PromiseKit.git",
